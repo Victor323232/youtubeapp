9dbc6b1de7ecd135c6797ac438e072f21b418e33
Victor323232 <victorhugoabc59@gmail.com>
2022-09-12 10:50:35 -0300

    ?, e nao obrigatorio

--- lib/model/Video.dart
+++ lib/model/Video.dart
@@ -1,9 +1,9 @@
 class Video{
-  String id;
-  String titulo;
-  String imagem;
-  String canal;
-  String descricao;
+  String? id;
+  String? titulo;
+  String? imagem;
+  String? canal;
+  String? descricao;
 
-  Video(this.id, this.titulo, this.imagem, this.canal, this.descricao);
+  Video({this.id, this.titulo, this.imagem, this.canal, this.descricao});
 }
