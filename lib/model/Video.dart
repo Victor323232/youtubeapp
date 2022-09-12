1ebd54e1ce1d9e28e5a84fe105b61a8e02a0df58
Victor323232 <victorhugoabc59@gmail.com>
2022-09-12 11:19:54 -0300

    conversao dados json para um modelo para do sistema

--- lib/model/Video.dart
+++ lib/model/Video.dart
@@ -6,4 +6,21 @@ class Video{
   String? descricao;
 
   Video({this.id, this.titulo, this.imagem, this.canal, this.descricao});
+  /*
+  static converterJson(Map<String, dynamic> json){ criar varias estancias
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"]["thumbnails"] ["high"] ["url"],
+      canal: json["snippet"]["channelId"],
+    );
+  }*/
+  factory Video.fromJson(Map<String, dynamic> json){ //criar um unica estancia de video
+  return Video(
+  id: json["id"]["videoId"],
+  titulo: json["snippet"]["title"],
+  imagem: json["snippet"]["thumbnails"] ["high"] ["url"],
+  canal: json["snippet"]["channelId"],
+    );
+  }
 }
--- lib/telas/Api.dart
+++ lib/telas/Api.dart
@@ -1,5 +1,7 @@
 import 'package:http/http.dart'as http;
 import 'dart:convert';
+
+import '../model/Video.dart';
 const CHAVES_YOUTUBE_API = "AIzaSyAIDnl6ze3OHMak5oLN1Qj7Fg8gOb45Xu8";
 const ID_CANAL = "UC6aMp6QGYgGEUdHvZBhZV7w";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
@@ -20,7 +22,15 @@ class Api {
     );
     if(response.statusCode==200){
       Map <String, dynamic> dadosJson = json.decode(response.body);
-      print("Resultado:"+ dadosJson["items"] [0] [""][""].toString());
+      List<Video>videos =dadosJson["items"].map<Video>( //melhor maneira de fazer
+          (map){  //convercao de maops para um video
+            return Video.fromJson(map);
+          }
+      ).toList();//convercao desse videos para uma lista;
+
+      /*for (var video in dadosJson["items"]){  maneira de fazer
+      }*/
+      print("Resultado:"+ dadosJson["items"] [0] ["snippet"]["title"].toString());
 
     }else{
 
