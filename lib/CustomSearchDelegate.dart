f4339117bff7452a51a5b24baecb19e08aae6878
Victor323232 <victorhugoabc59@gmail.com>
2022-09-13 07:43:54 -0300

    sugestao para pesquisa

--- lib/CustomSearchDelegate.dart
+++ lib/CustomSearchDelegate.dart
@@ -37,7 +37,24 @@ class CustomSearchDelegate extends SearchDelegate <String> {
   @override
   Widget buildSuggestions(BuildContext context) {
     // TODO: implement buildSuggestions
-    return Container();
+    List<String> lista = [];
+    if(query.isNotEmpty){
+      lista = [
+        "android", "victor hugo",
+      ];
+      return ListView.builder(
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(lista [index]),
+          );
+        },
+        itemCount: lista.length,
+      );
+    }else{
+      return Center(
+        child: Text("Nenhum resultado para a pesquisa"),
+      );
+    }
   }
 
 }
